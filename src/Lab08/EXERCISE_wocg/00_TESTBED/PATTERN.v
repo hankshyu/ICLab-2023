c0ab4c9cf5289d96c003c5001bf05cf177ccde2e
hankshyu <hankshyu@gmail.com>
2023-04-23 10:00:12 +0800

    update testbench

--- src/Lab08/EXERCISE_wocg/00_TESTBED/PATTERN.v
+++ src/Lab08/EXERCISE_wocg/00_TESTBED/PATTERN.v
@@ -1,3 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////
+//  Engineer:        Tzu-Han Hsu
+//  Create Date:     04/21/2023 7:11:39 PM
+//  Module Name:     PATTERN
+//  Project Name:    ICLAB Lab08
+//  HDL(Version):    Verilog-2001
+//
+//  Dependencies:    none
+//
+//////////////////////////////////////////////////////////////////////////////////
+//  Description:     1. Randomize testbench data with pretty output colours
+//
+//////////////////////////////////////////////////////////////////////////////////
+//  Revision:
+//////////////////////////////////////////////////////////////////////////////////
+//  License information:
+//
+//  This software is released under the BSD-3-Clause Licence,
+//  see https://opensource.org/licenses/BSD-3-Clause for details.
+//  In the following license statements, "software" refers to the
+//  "source code" of the complete hardware/software system.
+//
+//  Copyright 2023,
+//                    IRIS Lab
+//                    Graduate Institute of Electronics Engineering
+//                    National Taiwan University
+//                    Taipei, Taiwan.
+//
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////////
 module PATTERN(
 	// Output signals
 	clk,
